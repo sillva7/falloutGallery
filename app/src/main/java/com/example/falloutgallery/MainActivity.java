da51e3b6d904288bd3646b01a6f50262a2111770
Sergey <isi2lva@gmail.com>
2020-06-25 16:06:31 +0500

    Just RecView

--- app/src/main/java/com/example/falloutgallery/MainActivity.java
+++ app/src/main/java/com/example/falloutgallery/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.falloutgallery;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.app.Activity;
 import android.content.Intent;
@@ -16,17 +18,40 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.falloutgallery.classes.CardItem;
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter adapter;
+    private RecyclerView.LayoutManager layoutManager;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<CardItem> cardItems = new ArrayList<>();
+
+        cardItems.add(new CardItem(R.drawable.qqqqq, "001"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "002"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "003"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "004"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "005"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "006"));
+        cardItems.add(new CardItem(R.drawable.qqqqq, "007"));
+
+        recyclerView = findViewById(R.id.recyclerView);
+        adapter = new RecyclerViewAdapter(cardItems);
+        layoutManager = new LinearLayoutManager(this);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(layoutManager);
 
     }
 
